a08d0171c3a7dc200b841ad00e2e745abbee34b7
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-07-08 15:58:58 -0300

    chore: install and setupe biome

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {App} from './app.tsx'
+import { App } from './app.tsx'
 
+// biome-ignore lint/style/noNonNullAssertion: mandatory for createRoot
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
 )
