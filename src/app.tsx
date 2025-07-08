cfd84c6bec2592643e3504565123a64490a217d6
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-07-08 16:19:28 -0300

    feat:adding and configure shadcn

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,14 @@
+import { Button } from './components/ui/button'
+
 export function App() {
-  return <div className="bg-zinc-950 text-white">Hello World</div>
+  return (
+    <div>
+      <Button variant="default">Teste</Button>
+      <Button variant="destructive">Teste</Button>
+      <Button variant="ghost">Teste</Button>
+      <Button variant="link">Teste</Button>
+      <Button variant="outline">Teste</Button>
+      <Button variant="secondary">Teste</Button>
+    </div>
+  )
 }
